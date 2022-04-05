6b912c3ff611d529c310a119ab11ce0610a91743
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-04-04 23:30:25 -0300

    :construction: build

---
+++ dist/JavaScript/productsData.js
@@ -0,0 +1,82 @@
+// Available filters: new, coffee, dessert
+
+const newProducts = [
+  {
+    filter: 'new',
+    img: './Images/newproduct1.png',
+    price: 'R$6,00',
+    name: 'Cookie',
+  },
+  {
+    filter: 'new',
+    img: './Images/newproduct2.png',
+    price: 'R$12,00',
+    name: 'Croissant',
+  },
+  {
+    filter: 'new',
+    img: './Images/newproduct3.png',
+    price: 'R$10,00',
+    name: 'Pretzel',
+  },
+];
+
+const coffees = [
+  {
+    filter: 'coffee',
+    img: './Images/coffee1.png',
+    price: 'R$5,00',
+    name: 'Café expresso',
+  },
+  {
+    filter: 'coffee',
+    img: './Images/coffee2.png',
+    price: 'R$7,00',
+    name: 'Café com leite',
+  },
+  {
+    filter: 'coffee',
+    img: './Images/coffee3.png',
+    price: 'R$9,00',
+    name: 'Cappuccino',
+  },
+  {
+    filter: 'coffee',
+    img: './Images/coffee4.png',
+    price: 'R$10,00',
+    name: 'Macchiato',
+  },
+];
+
+const desserts = [
+  {
+    filter: 'dessert',
+    img: './Images/dessert1.png',
+    price: 'R$8,00',
+    name: 'Bolo de morango',
+  },
+  {
+    filter: 'dessert',
+    img: './Images/dessert2.png',
+    price: 'R$14,00',
+    name: 'Croissant de chocolate',
+  },
+  {
+    filter: 'dessert',
+    img: './Images/dessert3.png',
+    price: 'R$14,00',
+    name: 'Panqueca',
+  },
+  {
+    filter: 'dessert',
+    img: './Images/dessert4.png',
+    price: 'R$9,00',
+    name: 'Bolo de leite',
+  },
+];
+
+export {
+  newProducts,
+  coffees,
+  desserts,
+};
