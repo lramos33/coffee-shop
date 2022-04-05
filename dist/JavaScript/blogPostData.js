f441a27f6a45b6745aa15740dd730826b84554da
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-04-05 00:53:29 -0300

    :construction: build

---
+++ dist/JavaScript/blogPostData.js
@@ -0,0 +1,20 @@
+const blogPosts = [
+  {
+    img: './Images/blog1.png',
+    link: '#home',
+    title: 'Como armazenar o café?',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Etiam cursus a odio sed imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra. ',
+    comments: 13,
+    views: '24,6k',
+  },
+  {
+    img: './Images/blog2.png',
+    link: '#home',
+    title: '12 benefícios de beber café',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Etiam cursus a odio sed imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra. ',
+    comments: 35,
+    views: '128,2k',
+  },
+];
+
+export default blogPosts;
